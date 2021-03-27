c1c89eae9969409b35d050e12d89c1d971668dcc
Anshul Saboo <developeranshulsaboo@gmail.com>
2021-03-27 09:03:46 +0530

    Source Hackerearth

---
+++ IO_Jaddoo_hates_numbers.py
@@ -0,0 +1,10 @@
+'''
+Author: Anshul Saboo
+Date: 27/03/2021
+Problem: IO_Jaddoo Hates Numbers
+Platform: HackerEarth
+
+'''
+#Declararing Number and using the ASCII value of letter F
+number = ord("F") * 6 #ASCII number of F is 70
+print(number)
